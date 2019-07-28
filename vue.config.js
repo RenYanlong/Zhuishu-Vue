9acfbe829e7fa9e3e6f2f54b4430670712155482
Renyl <ren.yl@live.com>
2019-07-28 13:19:42 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const path = require("path");
+
 module.exports = {
+  // 调整webpack配置
+  configureWebpack: {
+    plugins: [
+      
+    ]
+  },
   pluginOptions: {
     "style-resources-loader": {
       patterns: ["src/assets/less/color.less"],
